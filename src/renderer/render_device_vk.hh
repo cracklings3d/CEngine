58611fd37a49d17ab2c5f91c8d73f4089c9a68de
cracklings3d <cr@crack.localdomain>
2019-07-09 19:30:10 +0800

    190709

--- src/renderer/render_device_vk.hh
+++ src/renderer/render_device_vk.hh
@@ -13,7 +13,10 @@ public:
 
   virtual void initialize() override { create_device(); }
   virtual void flush() override {}
-  virtual void terminate() override { destroy_instance(); }
+  virtual void terminate() override {
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
+  }
 
   virtual void render(const renderable &item) const override {}
 
@@ -33,12 +36,12 @@ protected:
   }
 
   void create_device() {
-    std::vector<VkPhysicalDevice> physical_devices{};
-    uint32_t                      physical_device_count;
+    uint32_t physical_device_count;
     vkEnumeratePhysicalDevices(instance, &physical_device_count, nullptr);
-    physical_devices.reserve(physical_device_count);
+    VkPhysicalDevice physical_devices[16]{};
+
     vkEnumeratePhysicalDevices(instance, &physical_device_count,
-                               &physical_devices[0]);
+                               physical_devices);
     // for (int i = 0; i < physical_device_count; i++) {
     //   VkPhysicalDeviceProperties physical_device_property;
     //   vkGetPhysicalDeviceProperties(physical_devices[i],
@@ -56,27 +59,29 @@ protected:
     // vkGetPhysicalDeviceProperties(physical_devices[1],
     // 															&physical_device_property1);
 
-    for (auto iter = physical_devices.begin(); iter == physical_devices.end();
-         iter++) {
+    for (int i = 0; i < physical_device_count; i++) {
       VkPhysicalDeviceProperties physical_device_property;
-      vkGetPhysicalDeviceProperties(*iter, &physical_device_property);
-      switch (physical_device_property.vendorID) {
-        if (physical_device_property.vendorID == 4318 // nvidia
-            || physical_device_property.vendorID ==
-                   4318 // change to something else
-        ) {
-          VkDeviceCreateInfo device_create_info = {};
-          device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-          VkResult create_device_result =
-              vkCreateDevice(*iter, &device_create_info, nullptr, &device);
-          Expects(create_device_result == VK_SUCCESS);
-          return;
-        }
+      vkGetPhysicalDeviceProperties(physical_devices[i],
+                                    &physical_device_property);
+      if (physical_device_property.vendorID == 4318 // nvidia
+      ) {
+        VkDeviceCreateInfo device_create_info = {};
+        device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        VkPhysicalDeviceFeatures pd_features;
+        vkGetPhysicalDeviceFeatures(physical_devices[i], &pd_features);
+        device_create_info.pEnabledFeatures = &pd_features;
+				VkDeviceQueueCreateInfo device_queue_create_info;
+				device_queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+				device_queue_create_info.queueCount
+        VkResult create_device_result = vkCreateDevice(
+            physical_devices[i], &device_create_info, nullptr, &device);
+        Expects(create_device_result == VK_SUCCESS);
+        return;
       }
-      printf("Found no GPU with Vulkan support! Check your Graphics card and "
-             "driver.");
     }
+    printf("Found no GPU with Vulkan support! Check your Graphics card and "
+           "driver.");
   }
-  void destroy_instance() { vkDestroyInstance(instance, nullptr); }
+  void destroy_instance() {}
 };
 } // namespace CEngine
