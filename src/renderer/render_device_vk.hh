8faa7bf176dcb782c7067d7d07d566e65d7389db
cracklings3d <cr@crack.localdomain>
2019-07-11 10:30:07 +0800

    device & queue creation

--- src/renderer/render_device_vk.hh
+++ src/renderer/render_device_vk.hh
@@ -15,19 +15,26 @@ public:
 
   virtual ~render_device_vk() override {}
 
-  virtual void initialize() override { }
+  virtual void initialize() override {}
   virtual void flush() override {}
 
   virtual void terminate() override {
-    if (device) device.destroy();
-    if (instance) instance.destroy();
+    if (device)
+      device.destroy();
+    if (instance)
+      instance.destroy();
   }
 
   virtual void render(const renderable &item) const override {}
 
 protected:
-  vk::Instance instance;
-  vk::Device   device;
+  vk::Instance       instance;
+  vk::PhysicalDevice physical_device;
+  vk::Device         device;
+  vk::Queue          gqueue;
+  float              gq_priority = 1;
+  vk::Queue          cqueue;
+  float              cq_priority = .5;
 
   void create_instance() {
     vk::ApplicationInfo app_info{};
@@ -37,33 +44,62 @@ protected:
     app_info.pEngineName        = "CEngine";
     app_info.pApplicationName   = "CENGINE_TEST";
 
-    std::vector<const char*> debug_layer_name{"VK_LAYER_LUNARG_core_validation"};
-    vk::InstanceCreateInfo   instance_create_info{};
-    instance_create_info.pApplicationInfo    = &app_info;
-    instance_create_info.enabledLayerCount   = debug_layer_name.size();
-    instance_create_info.ppEnabledLayerNames = &debug_layer_name[0];
+    std::vector<const char *> debug_layer_name{
+        "VK_LAYER_LUNARG_core_validation"};
+    vk::InstanceCreateInfo instance_ci{};
+    instance_ci.pApplicationInfo    = &app_info;
+    instance_ci.enabledLayerCount   = debug_layer_name.size();
+    instance_ci.ppEnabledLayerNames = &debug_layer_name[0];
 
-    instance = vk::createInstance(instance_create_info);
+    instance = vk::createInstance(instance_ci);
   }
 
   void create_device() {
-    std::vector<vk::PhysicalDevice> physical_devices
-        = instance.enumeratePhysicalDevices();
+    std::vector<vk::PhysicalDevice> physical_devices =
+        instance.enumeratePhysicalDevices();
 
     for (auto pd : physical_devices) {
       vk::PhysicalDeviceProperties pd_property = pd.getProperties();
       if (pd_property.vendorID == 4318 // nvidia
       ) {
-        auto pd_queue_family_properties = pd.getQueueFamilyProperties();
+        physical_device = pd;
+        std::vector<vk::QueueFamilyProperties> pd_queue_family_properties =
+            pd.getQueueFamilyProperties();
+
+        std::vector<vk::DeviceQueueCreateInfo> dev_q_ci{};
+        dev_q_ci.reserve(2);
+        vk::DeviceQueueCreateInfo graphics_queue_ci{};
+        vk::DeviceQueueCreateInfo compute_queue_ci{};
+
+        int i = 0;
+        for (const auto &qfprops : pd_queue_family_properties) {
+          if (!gqueue && qfprops.queueFlags & vk::QueueFlagBits::eGraphics) {
+            graphics_queue_ci.queueFamilyIndex = i;
+            graphics_queue_ci.queueCount       = 1;
+            graphics_queue_ci.pQueuePriorities = &gq_priority;
+            graphics_queue_ci.flags = vk::DeviceQueueCreateFlagBits::eProtected;
+          }
+          if (!cqueue && qfprops.queueFlags & vk::QueueFlagBits::eCompute) {
+            compute_queue_ci.queueFamilyIndex = i;
+            compute_queue_ci.queueCount       = 1;
+            compute_queue_ci.pQueuePriorities = &cq_priority;
+            compute_queue_ci.flags = vk::DeviceQueueCreateFlagBits::eProtected;
+          }
+          i++;
+        }
+				dev_q_ci.push_back(graphics_queue_ci);
+				dev_q_ci.push_back(compute_queue_ci);
 
         vk::DeviceCreateInfo device_create_info = {};
+				device_create_info.pQueueCreateInfos = &dev_q_ci[0];
+				device_create_info.queueCreateInfoCount = dev_q_ci.size();
 
+				device = pd.createDevice(device_create_info);
         return;
       }
     }
-    printf(
-      "Found no GPU with Vulkan support!"
-      " Check your Graphics card and driver.");
+    printf("Found no GPU with Vulkan support!"
+           " Check your Graphics card and driver.");
   }
 };
 
